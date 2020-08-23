6712c55d65d490934b21221575ac75f2e0cfdc4a
Catherine <catherine@madstreetden.com>
2020-08-23 19:52:27 +0530

    fINAL

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&mi*h#1n%in)@4qe((w617vqta@o5a2i#_@udx5a*0t5t7ilu&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1']
 
@@ -122,3 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+STATIC_ROOT='static/'

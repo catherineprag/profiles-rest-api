3738450d4ea86824bab04d71b5d4930adeea868c
Catherine <catherine@madstreetden.com>
2020-08-23 18:50:27 +0530

    Permissions

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,28 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allows users to update their own status."""
+
+    def has_object_permission(self, request, view, obj):
+        """Check if the user is trying to edit his/her own status."""
+        print('Entered has_object_permission')
+        print('request.method = {}'.format(request.method))
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        print('obj.user_profile.id = {}'.format(obj.user_profile.id))
+        print('request.user.id = {}'.format(request.user.id))
+        return obj.user_profile.id == request.user.id
